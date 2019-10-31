9f72b092bbc46e29841dcba951ab898494bf19fc
Tommy Vinyard <tpvinyard@gmail.com>
2019-10-30 20:17:14 -0500

    adding db, config folders as well as package.json

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Honey BBQ Chicken Strip Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Bacon Burger');
