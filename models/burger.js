33554ae75c7f7dd2a9e263c6e6d1c2f6c8fd3970
Tommy Vinyard <tpvinyard@gmail.com>
2019-10-30 22:56:42 -0500

    adding models and start of controllers

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,22 @@
 const orm = require('../config/orm.js');
 
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burger", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    createOne: function(cols, vals, cb) {
+      orm.createOne("burger", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burger", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  module.exports = burger;
