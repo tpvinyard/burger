2a577afe65840d240b8af9ad4ab0852d97dd405a
Tommy Vinyard <tpvinyard@gmail.com>
2019-10-31 17:57:42 -0500

    adding burger_controller routes

--- controllers/burgers_contoller.js
+++ controllers/burgers_contoller.js
@@ -1,2 +1,46 @@
 const express = require('express');
 const burger = require('../models/burger.js');
+
+const router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burger: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(204).end();
+  
+      }
+    );
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
